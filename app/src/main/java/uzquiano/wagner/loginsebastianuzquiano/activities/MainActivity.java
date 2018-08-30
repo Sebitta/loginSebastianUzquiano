98887374ceee78381ec5686ad22997bdd610de10
Sebitta <wagner.uzquiano@alumnos.uta.cl>
2018-08-30 14:58:55 -0300

    Initial commit

---
+++ app/src/main/java/uzquiano/wagner/loginsebastianuzquiano/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package uzquiano.wagner.loginsebastianuzquiano.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uzquiano.wagner.loginsebastianuzquiano.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
